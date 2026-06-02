1fa43cf699dafab92af4e84bb6f550798bb8e24e
rtofa <ryantofanini@gmail.com>
2026-06-01 23:29:53 -0300

    fix(CI/CD): Adicionado URL de fallback ao build

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_API_URL || 'https://api.vitallyoficial.com.br'}/api/:path*`,
       },
     ];
   },
