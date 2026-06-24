4cf9a619f8c43eb3e8799df67a9d81bd441973ba
rtofa <ryantofanini@gmail.com>
2026-06-24 11:07:37 -0300

    chore: atualizando endereço interno

--- next.config.js
+++ next.config.js
@@ -8,13 +8,12 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        // Garanta que o source corresponde ao prefixo que o seu frontend usa
-        source: '/api/v1/:path*', 
-        // O DESTINO deve ser o endereço HTTP interno dentro do Docker
-        destination: 'http://vitally-backend:8080/api/v1/:path*', 
+        source: '/api/:path*',
+        // DESTINO FIXO (Endereço interno do Docker)
+        destination: 'http://vitally-backend:8080/api/:path*', 
       },
     ];
   },
 };
 
-  module.exports = nextConfig;
+module.exports = nextConfig;
